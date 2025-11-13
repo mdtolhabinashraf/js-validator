eac1821266af1ffdefdf0a9de4ff7ffc81d305ae
Md. Tolha Bin Ashraf <172221818+mdtolhabinashraf@users.noreply.github.com>
2025-11-13 12:49:28 +0600

    feat: initialize js-validator library with TypeScript support

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config({
+  files: ["src/**/*.ts"],
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+});
